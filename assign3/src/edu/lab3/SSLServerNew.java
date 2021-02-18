bc6aebcb8df87080460151b593d98d19621a2162
Demeter Tamas <demeter44@gitlab.com>
2021-02-18 08:46:26 +0200

    lab3

---
+++ assign3/src/edu/lab3/SSLServerNew.java
@@ -0,0 +1,120 @@
+package edu.lab3;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.lang.*;
+
+import com.sun.net.httpserver.HttpsServer;
+
+import java.security.KeyStore;
+import javax.net.ssl.*;
+
+import com.sun.net.httpserver.*;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpsExchange;
+
+public class SSLServerNew {
+
+    public static class MyHandler implements HttpHandler {
+
+        public static String readFile() {
+            try {
+                File myObj = new File("body.txt");
+                Scanner myReader = new Scanner(myObj);
+                StringBuilder sb = new StringBuilder();
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    sb.append(data);
+                }
+                myReader.close();
+                System.out.println(sb.toString());
+                return sb.toString();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+                return "";
+            }
+        }
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = readFile();
+            HttpsExchange httpsExchange = (HttpsExchange) t;
+            System.out.println("*");
+            //Probably something to do here
+            System.out.println(Arrays.toString(httpsExchange.getSSLSession().getLocalCertificates()));
+            System.out.println("*");
+            t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            t.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        try {
+            // setup the socket address
+            InetSocketAddress address = new InetSocketAddress(443);
+
+            // initialise the HTTPS server
+            HttpsServer httpsServer = HttpsServer.create(address, 0);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            // initialise the keystore
+            char[] password = "password".toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            FileInputStream fis = new FileInputStream("testkeyserver.jks");
+            ks.load(fis, password);
+
+
+            // setup the key manager factory
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            // setup the trust manager factory
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
+
+            // setup the HTTPS context and parameters
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            httpsServer.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialise the SSL context
+                        SSLContext context = getSSLContext();
+                        SSLEngine engine = context.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // Set the SSL parameters
+                        SSLParameters sslParameters = context.getSupportedSSLParameters();
+                        params.setSSLParameters(sslParameters);
+
+                    } catch (Exception ex) {
+                        System.out.println("Failed to create HTTPS port");
+                    }
+                }
+            });
+            httpsServer.createContext("/test", new MyHandler());
+            httpsServer.setExecutor(null);
+            httpsServer.start();
+
+        } catch (Exception exception) {
+            System.out.println("Failed to create HTTPS server on port " + 443 + " of localhost");
+            exception.printStackTrace();
+
+        }
+    }
+
+}
+
